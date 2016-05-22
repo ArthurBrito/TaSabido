0cf14eccd7e38fb5fe00bd4d97859c0ac07f4ca8
Thiago Nóbrega <thiago_nrodrigues@yahoo.com.br>
2016-05-21 21:09:29 -0300

    Mudancas na ActionBar do SubtopicosActivity

--- app/src/main/java/br/ufc/engsoftware/tasabido/SubtopicoActivity.java
+++ app/src/main/java/br/ufc/engsoftware/tasabido/SubtopicoActivity.java
@@ -1,13 +1,13 @@
 package br.ufc.engsoftware.tasabido;
 
-import android.content.Context;
+
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
 
-import br.ufc.engsoftware.models.Materia;
 
 public class SubtopicoActivity extends AppCompatActivity {
 
@@ -16,6 +16,7 @@ public class SubtopicoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subtopico);
 
@@ -24,8 +25,11 @@ public class SubtopicoActivity extends AppCompatActivity {
         materia = intent.getStringExtra("MATERIA");
         id_materia = intent.getIntExtra("ID", 0);
 
-        TextView tvMateria = (TextView) findViewById(R.id.tv_materia);
-        tvMateria.setText(materia);
+        ActionBar ab = getSupportActionBar();
+        ab.setTitle(materia);
+        ab.setDisplayHomeAsUpEnabled(true);
+
+
     }
 
     public void onClickMonitorias(View view){
@@ -43,5 +47,16 @@ public class SubtopicoActivity extends AppCompatActivity {
         //intent.putExtra("ID", item.getId());
 
         startActivity(intent);
+    }
+
+    // Seta ação do botão de voltar na ActionBar
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
