76c0da376daf49e052a128614d88a0c906ff7f4d
limaneto <neto_pereira1@hotmail.com>
2016-05-26 14:15:48 -0300

    tudo mais atualizado

--- app/src/main/java/br/ufc/engsoftware/tasabido/CriarMonitoriaActivity.java
+++ app/src/main/java/br/ufc/engsoftware/tasabido/CriarMonitoriaActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -15,6 +16,8 @@ import java.util.Vector;
 
 import br.ufc.engsoftware.BDLocalManager.SubtopicoBDManager;
 import br.ufc.engsoftware.auxiliar.Utils;
+import br.ufc.engsoftware.fragments.DatePickerFragment;
+import br.ufc.engsoftware.fragments.TimePickerFragment;
 import br.ufc.engsoftware.models.Subtopico;
 
 public class CriarMonitoriaActivity extends AppCompatActivity {
@@ -30,8 +33,9 @@ public class CriarMonitoriaActivity extends AppCompatActivity {
         Intent intent = getIntent();
         materia = intent.getStringExtra("MATERIA");
         id_materia = intent.getIntExtra("ID", 0);
-    }
 
+        setSpinnerView(this, pegarSubtopicos(id_materia));
+    }
 
     //esse metodo acessa o banco de dados local e retorna todos os subtopicos referentes a materia que o usuario esta vendo
     public Vector<Subtopico> pegarSubtopicos(int id_materia){
@@ -40,6 +44,15 @@ public class CriarMonitoriaActivity extends AppCompatActivity {
         return subtopicos;
     }
 
+    public void escolherData(View view){
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public void escolherHorario(View view){
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
 
     // Seta as configurações do ListView
     public void setSpinnerView(Context view, Vector<Subtopico> vector){
@@ -51,7 +64,6 @@ public class CriarMonitoriaActivity extends AppCompatActivity {
         ArrayAdapter<Subtopico> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, subtopicos);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
-
     }
 
 }
